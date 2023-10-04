2e152bc30826734ef308d37b09f39735a0c6d687
Mariko Suzuki <marikosuzuki@foodtravel.co.jp>
2023-10-04 10:54:28 +0900

    populate spaceX launch data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,35 +1,101 @@
+const axios = require('axios');
+
 const launchesDatabase = require('./launches.mongo');
 const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
-  flightNumber: 100,
-  mission: 'Kepler Exploration X',
-  rocket: 'Explorer IS1',
-  launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442 b',
-  customers: ['ZTM', 'NASA'],
-  upcoming: true,
-  success: true,
+  flightNumber: 100, // flight_number
+  mission: 'Kepler Exploration X', // name
+  rocket: 'Explorer IS1', // rocket.name
+  launchDate: new Date('December 27, 2030'), // date_local
+  target: 'Kepler-442 b', // not applicable
+  customers: ['ZTM', 'NASA'], // payload.customers for each payload
+  upcoming: true, // upcoming
+  success: true, // success
 };
 
 saveLaunch(launch);
 
-async function loadLaunchData(){
-  console.log('Downloading launch data...')
+const SPACE_API_URL = 'https://api.spacexdata.com/v4/launches/query';
+
+async function populateLaunches(){
+ 
+  console.log('Downloading launch data...');
+  const response = await axios.post(SPACE_API_URL, {
+    query: {},
+    options: {
+      pagination: false,
+      populate: [
+        {
+          path: 'rocket',
+          select: {
+            name: 1,
+          },
+        },
+        {
+          path: 'payloads',
+          select: {
+            customers: 1,
+          },
+        },
+      ],
+    },
+    headers: {
+      'Accept-Encoding': 'application/json',
+    },
+  });
+
+  const launchDocs = response.data.docs;
+
+  for (const launchDoc of launchDocs){
+    const payloads = launchDoc['payloads']
+    const customers = payloads.flatMap((payload) => {
+      payload['customers']
+    })
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket']['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+      customers: customers
+    };
+  
+    console.log(`${launch.flightNumber} ${launch.mission}`)
+
+    // TODO: populate launches collection...
+  } 
+}
+
+async function loadLaunchData() {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1, 
+    rocket: 'Falcon 1',
+    mission: 'FalconSat',
+  });
+if(firstLaunch){
+  console.log('Launch data already loaded')
+} else {
+  await populateLaunches()
+}
+
+}
+
+async function findLaunch(filter){
+  return await launchesDatabase.findOne(filter)
 }
 
 async function existsLaunchWithId(launchId) {
-  return await launchesDatabase.findOne({
+  return await findLaunch({
     flightNumber: launchId,
   });
 }
 
 async function getLatestFlightNumber() {
-  const latestLaunch = await launchesDatabase
-    .findOne()
-    .sort('-flightNumber');
+  const latestLaunch = await launchesDatabase.findOne().sort('-flightNumber');
 
   if (!latestLaunch) {
     return DEFAULT_FLIGHT_NUMBER;
@@ -39,8 +105,7 @@ async function getLatestFlightNumber() {
 }
 
 async function getAllLaunches() {
-  return await launchesDatabase
-    .find({}, { '_id': 0, '__v': 0 });
+  return await launchesDatabase.find({}, { _id: 0, __v: 0 });
 }
 
 async function saveLaunch(launch) {
@@ -52,15 +117,19 @@ async function saveLaunch(launch) {
     throw new Error('No matching planet found');
   }
 
-  await launchesDatabase.findOneAndUpdate({
-    flightNumber: launch.flightNumber,
-  }, launch, {
-    upsert: true,
-  });
+  await launchesDatabase.findOneAndUpdate(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
+  );
 }
 
 async function scheduleNewLaunch(launch) {
-  const newFlightNumber = await getLatestFlightNumber() + 1;
+  const newFlightNumber = (await getLatestFlightNumber()) + 1;
 
   const newLaunch = Object.assign(launch, {
     success: true,
@@ -73,14 +142,17 @@ async function scheduleNewLaunch(launch) {
 }
 
 async function abortLaunchById(launchId) {
-  const aborted = await launchesDatabase.updateOne({
-    flightNumber: launchId,
-  }, {
-    upcoming: false,
-    success: false,
-  });
+  const aborted = await launchesDatabase.updateOne(
+    {
+      flightNumber: launchId,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
+  );
 
-  return aborted.modifiedCount=== 1;
+  return aborted.modifiedCount === 1;
 }
 
 module.exports = {
